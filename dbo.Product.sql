edd8dea5a18a5affb54c12ef43b1398bc7291fb9
Erik Lagnefeldt <e.lagnefeldt@gmail.com>
2023-10-30 14:53:51 +0100

    Add project files.

---
+++ dbo.Product.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX) NOT NULL,
+    [Price]       INT            NOT NULL,
+    [Orderdate]   DATETIME2 (7)  NOT NULL,
+    [Category]    NVARCHAR (MAX) NOT NULL,
+    [Shelf]       NVARCHAR (MAX) NOT NULL,
+    [Count]       INT            NOT NULL,
+    [Description] NVARCHAR (MAX) NOT NULL,
+    [Limited] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
